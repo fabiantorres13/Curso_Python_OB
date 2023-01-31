ce952aac305d9822efc0cd5ac908a027c0f56cae
Fabian Torres <programacioncursos2022@gmail.com>
2023-01-31 16:44:34 -0500

    Agregué ejercicio 2

---
+++ EJERCICIO_TEMA2/Ejercicio2.py
@@ -0,0 +1,8 @@
+# ENUNCIADO:
+# Modifica la variable del anterior ejercicio en la consola de python 
+# y después muestrala por consola para ver la modificación de la variable.
+
+saludo = "Hola mundo!"
+print(f'Saludo: {saludo}')
+saludo = input("Modificar saludo: ")
+print(f'Nuevo saludo: {saludo}')
