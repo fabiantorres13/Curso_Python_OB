0723434f3c0c77136b183ef060b0bd9a3a86075a
Fabian Torres <programacioncursos2022@gmail.com>
2023-02-02 16:39:34 -0500

    Agregé el Ejercicio 7.1

---
+++ EJERCICIO_TEMA7/EJERCICIO1/Ejercicio1.py
@@ -0,0 +1,13 @@
+'''
+ENUNCIADO:
+1. En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: 
+sumar, restar, multiplicar y dividir.
+2. Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. 
+Los resultados tenéis que mostrarlos por consola.
+'''
+import operaciones 
+
+print("Suma: ", operaciones.sumar(4,5))
+print("Resta: ", operaciones.restar(4,5))
+print("Multiplicación: ", operaciones.multiplicar(4,5))
+print("División: ", operaciones.dividir(4,5))
