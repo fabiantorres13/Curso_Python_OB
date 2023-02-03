ad8d4faae01b14f9a903a94e114d743140a3e5ba
Fabian Torres <programacioncursos2022@gmail.com>
2023-02-03 11:25:04 -0500

    Cambios no guardados

--- EJERCICIO_TEMA8/EjERCICIO1/Ejercicio1.py
+++ EJERCICIO_TEMA8/EjERCICIO1/Ejercicio1.py
@@ -1,3 +1,6 @@
+# ENUNCIADO -> En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, 
+# lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.
+
 f = open('primerFichero.txt', 'w')
 animales =['Perro\n', 
             'Gato\n',
