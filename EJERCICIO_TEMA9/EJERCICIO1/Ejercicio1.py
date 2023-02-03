5454e97dd34c0e7fc0bc8e06fff4ad08ebad39db
Fabian Torres <programacioncursos2022@gmail.com>
2023-02-03 16:41:20 -0500

    Agregué Ejercicio 1

---
+++ EJERCICIO_TEMA9/EJERCICIO1/Ejercicio1.py
@@ -0,0 +1,9 @@
+'''
+ENUNCIADO:
+Crea un script que le pida al usuario una lista de países (separados por comas). 
+Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set). 
+Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.
+'''
+valor = input("Ingresa una lista de paises(Sepáralos por comas): ")
+listaPaises = valor.split(',')
+print(sorted(set(listaPaises)))
