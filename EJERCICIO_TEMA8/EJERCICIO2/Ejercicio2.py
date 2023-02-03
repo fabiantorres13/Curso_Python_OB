6be7e492856b33dca1e98af32cd180b28668a0f7
Fabian Torres <programacioncursos2022@gmail.com>
2023-02-03 15:16:17 -0500

    Arreglé el Ejercicio 2

--- EJERCICIO_TEMA8/EJERCICIO2/Ejercicio2.py
+++ EJERCICIO_TEMA8/EJERCICIO2/Ejercicio2.py
@@ -1,12 +1,30 @@
+# ENUNCIADO -> En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo, 
+# haréis un objeto de ella, lo guardaréis en un archivo y luego lo cargamos.
+
 import pickle
 
 class Vehiculo:
+    tipo = ""
+    recorrido = 0.0
+    velocidad = 0
     
     def __init__(self,tipo,recorrido,velocidad):
-        print (f'El vehiculo de tipo {tipo} ha comenzado a andar, debe recorrer {recorrido} kilometros a una velocidad de {velocidad} km/h')
+        self.tipo = tipo
+        self.recorrido = recorrido
+        self.velocidad = velocidad
+    
+    def andar(self):
+        print (f'El vehiculo de tipo {self.tipo} ha comenzado a andar, debe recorrer {self.recorrido} kilometros a una velocidad de {self.velocidad} km/h')
     
 
+vehiculo = Vehiculo("Carro",100,80)
 
-f = open('Vehiculo.dat', 'rb')
-vehiculo = pickle.load(f)
+# Serializando...
+f = open('Vehiculo.picl', 'wb')
+pickle.dump(vehiculo,f)
 f.close()
+
+# Deserializando...
+f = open('Vehiculo.picl', 'rb')
+reconstrucionObjeto = pickle.load(f)
+reconstrucionObjeto.andar()
